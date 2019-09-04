8696ae0cbcee01ea00e9da7c7863066c5b642a27
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-09-04 14:51:09 +0800

    Updated "Users By Signup Month" base query.

--- base_queries/group users by signup month.sql
+++ base_queries/group users by signup month.sql
@@ -1,20 +1,22 @@
 /*  @Doc
-    This query, displays 
+    This base query, displays 
     and also the exact date that they were created in as the table's first attribute.
-    
-    
 */
+with base AS (
+    SELECT
+    DATE(TIMESTAMP_SECONDS(created_at___seconds)) AS created_date,
+    *
+    FROM
+    `pslove-usa.users_data_for_analytics.users`
+    WHERE
+    --   TIMESTAMP_DIFF(CURRENT_TIMESTAMP(), TIMESTAMP_SECONDS(created_at___seconds), ) < 2
+
+    -- all users who were created in the past 2 Months from the current month,
+    --     If current month is MAY, then all users who signed up in MAY and APR will show up.
+    --   AND DATE_DIFF(DATE(CURRENT_TIMESTAMP()), DATE(TIMESTAMP_SECONDS(created_at___seconds)), MONTH) < 2
+    ORDER BY
+    created_at___seconds DESC)
 SELECT
-  DATE(TIMESTAMP_SECONDS(created_at___seconds)) AS created_date,
-  *
+    *
 FROM
-  `pslove-usa.users_data_for_analytics.users`
-WHERE
---   TIMESTAMP_DIFF(CURRENT_TIMESTAMP(), TIMESTAMP_SECONDS(created_at___seconds), ) < 2
-
-
--- all users who were created in the past 2 Months from the current month,
---     If current month is MAY, then all users who signed up in MAY and APR will show up.
---   AND DATE_DIFF(DATE(CURRENT_TIMESTAMP()), DATE(TIMESTAMP_SECONDS(created_at___seconds)), MONTH) < 2
-ORDER BY
-  created_at___seconds DESC
+    base
